27c185c2986e8a071929cbc7ec5b6c54f4dfe74a
ZhangWen <zhw2101024@gmail.com>
2025-12-24 12:29:35 +0800

    handle nospace situation

--- src/parser/content.rs
+++ src/parser/content.rs
@@ -3,7 +3,7 @@ use regex::Regex;
 use std::io::{Error, ErrorKind};
 
 fn parse_date(line: &str) -> Result<String, Error> {
-    let date_re = Regex::new(r"^\b播出日期\s+(\d{4}).*(\d{2}).*(\d{2})$").unwrap();
+    let date_re = Regex::new(r"^\b播出日期\s*(\d{4}).*(\d{2}).*(\d{2})$").unwrap();
 
     let Some((_, [year, month, day])) = date_re.captures(line).map(|caps| caps.extract()) else {
         let err = Error::new(
@@ -18,7 +18,7 @@ fn parse_date(line: &str) -> Result<String, Error> {
 }
 
 fn parse_list(line: &str) -> Result<(u32, u32, &str), Error> {
-    let list_re = Regex::new(r"^\b(\d{2})\b\s*:\s*\b(\d{2})\s(.+)\s*$").unwrap();
+    let list_re = Regex::new(r"^\b(\d{2})\b\s*:\s*\b(\d{2})\s*(.+)\s*$").unwrap();
 
     let Some((_, [hour, minute, name])) = list_re.captures(line).map(|caps| caps.extract()) else {
         let err = Error::new(
@@ -91,24 +91,13 @@ mod tests {
         assert_eq!(parse_date("播出日期 2025/12/15").unwrap(), "2025/12/15");
         assert_eq!(parse_date("播出日期  2025/12/15").unwrap(), "2025/12/15");
         assert_eq!(parse_date("播出日期 2025/ 12 /15").unwrap(), "2025/12/15");
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_parse_date_nospace() {
         assert_eq!(parse_date("播出日期2025/12/15").unwrap(), "2025/12/15");
     }
 
     #[test]
     fn test_parse_list() {
         assert_eq!(parse_list("05:30 节目预告").unwrap(), (05, 30, "节目预告"));
-
         assert_eq!(parse_list("05: 30 节目预告").unwrap(), (05, 30, "节目预告"));
-    }
-
-    #[test]
-    #[should_panic]
-    fn test_parse_list_nospace() {
         assert_eq!(parse_list("05:30节目预告").unwrap(), (05, 30, "节目预告"));
     }
 
