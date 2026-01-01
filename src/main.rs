028e1de2589c53b6850424f1a8aa343540e74003
Zhang Wen <zhw2101024@gmail.com>
2026-01-01 15:28:30 +0800

    add gpui

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,16 @@
+mod ui;
+
 use parsefile_lib::MyError;
 use parsefile_lib::monitor;
 use parsefile_lib::parse_file;
 
 use std::path::Path;
 
+use ui::hello;
+
 fn main() {
     monitor();
+    hello();
     let path = Path::new("1.txt");
     match parse_file(path) {
         Ok(number) => {
