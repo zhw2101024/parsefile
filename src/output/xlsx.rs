7718b9df4fd798d0a07d2c79a9931a3618ce9f28
ZhangWen <zhw2101024@gmail.com>
2026-01-06 19:57:13 +0800

    use path instead of str

--- src/output/xlsx.rs
+++ src/output/xlsx.rs
@@ -2,7 +2,7 @@ use crate::{MyError, Record};
 use rust_xlsxwriter::Workbook;
 use std::{collections::BTreeMap, path::Path};
 
-pub fn write_map(record_map: &BTreeMap<String, Vec<Record>>, path: &str) -> Result<bool, MyError> {
+pub fn write_map(record_map: &BTreeMap<String, Vec<Record>>, path: &Path) -> Result<bool, MyError> {
     let ret = true;
 
     let mut workbook = Workbook::new();
@@ -22,12 +22,12 @@ pub fn write_map(record_map: &BTreeMap<String, Vec<Record>>, path: &str) -> Resu
         }
     }
 
-    let parent = Path::new(path)
+    let parent = path
         .parent()
-        .unwrap_or_else(|| panic!("unable to get parent for path: {}", path));
-    let prefix_os = Path::new(path)
+        .unwrap_or_else(|| panic!("unable to get parent for path: {}", path.to_string_lossy()));
+    let prefix_os = path
         .file_prefix()
-        .unwrap_or_else(|| panic!("unable to get prefix for path: {}", path));
+        .unwrap_or_else(|| panic!("unable to get prefix for path: {}", path.to_string_lossy()));
     let prefix = prefix_os
         .to_str()
         .unwrap_or_else(|| panic!("{}", prefix_os.display().to_string()));
