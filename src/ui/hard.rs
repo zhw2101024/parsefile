39383d01f703148c61a281e7bc17c8675fe0b955
Zhang Wen <zhw2101024@gmail.com>
2026-01-19 00:08:56 +0800

    improve observer

--- src/ui/hard.rs
+++ src/ui/hard.rs
@@ -2,10 +2,21 @@ use gpui::*;
 use gpui_component::{button::*, input::*, *};
 use gpui_component_assets::Assets;
 
+use std::collections::HashMap;
+
 use crate::parse_file;
 
+#[derive(Clone)]
+struct Warnings {
+    msgs: HashMap<String, Vec<String>>,
+}
+
+impl Global for Warnings {}
+
+#[derive(Clone)]
 pub struct HelloWorld {
     editor: Entity<InputState>,
+    display_text: String,
 }
 
 impl HelloWorld {
@@ -16,7 +27,10 @@ impl HelloWorld {
                 .multi_line(true)
                 .line_number(true)
         });
-        Self { editor }
+        Self {
+            editor,
+            display_text: String::default(),
+        }
     }
 }
 
@@ -25,7 +39,8 @@ impl Render for HelloWorld {
         let titlebar = TitleBar::new()
             .bg(gray(100))
             .child(div().child("节目单转换工具"));
-        div().child(titlebar).child(
+        println!("text: {}", self.display_text);
+        let foobar = div().child(titlebar).child(
             div()
                 .v_flex()
                 .gap_2()
@@ -37,37 +52,41 @@ impl Render for HelloWorld {
                         .primary()
                         .m_16()
                         .label("选择节目单")
-                        .on_click(cx.listener(|_, _, window, cx| {
+                        .on_click(cx.listener(|_, _, _, cx| {
                             let destination_prompt = cx.prompt_for_paths(PathPromptOptions {
                                 files: true,
                                 directories: false,
                                 multiple: false,
                                 prompt: None,
                             });
-                            window
-                                .spawn(cx, async move |_| {
-                                    let mut paths = destination_prompt.await.ok()?.ok()??;
-                                    let destination_file = paths.pop()?;
-                                    match parse_file(&destination_file) {
-                                        Ok(number) => {
-                                            println!("passed {} records", number);
-                                        }
-                                        Err(e) => println!("{}", e),
-                                    }
-                                    Some(())
-                                })
-                                .detach();
+                            cx.spawn(async move |entity, app| {
+                                let mut paths = destination_prompt.await.ok()?.ok()??;
+                                let destination_file = paths.pop()?;
+                                let text = parse_file(&destination_file).ok()?;
+                                entity
+                                    .update(app, |hello, context| {
+                                        hello.display_text = text;
+                                        context.notify();
+                                    })
+                                    .ok()
+                            })
+                            .detach();
                         })),
                 )
                 .child(Input::new(&self.editor).flex_1()),
-        )
+        );
+        for (name, warnings) in Warnings::global(cx).msgs.iter() {
+            println!("foobar: {name}, {}", warnings.len());
+        }
+        foobar.child(self.display_text.clone())
     }
 }
 
 fn draw(app: &mut App, window: &mut Window, handle: &Entity<HelloWorld>) {
     app.update_entity(handle, |this, cx| {
+        let text = this.display_text.clone();
         this.editor.update(cx, |state, cx| {
-            state.set_value("foobar", window, cx);
+            state.set_value(text, window, cx);
         })
     });
 }
@@ -76,6 +95,12 @@ pub fn hello() {
     let app = Application::new().with_assets(Assets);
 
     app.run(move |cx| {
+        Warnings::set_global(
+            cx,
+            Warnings {
+                msgs: HashMap::default(),
+            },
+        );
         // This must be called before using any GPUI Component features.
         gpui_component::init(cx);
 
